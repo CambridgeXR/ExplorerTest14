79dfa0b1532aaa73dfc66915278e57566d72a431
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-16 15:31:35 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,41 @@
-const CACHE_NAME = 'vr-explorer-v1';
-const OFFLINE_URLS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+const CACHE_NAME = 'vr-explorer-cache-v1';
+const urlsToCache = [
+  '/ExplorerTest14/index.html',
+  '/ExplorerTest14/manifest.json',
+  '/ExplorerTest14/sw.js',
+  '/ExplorerTest14/icons/icon-192.png',
+  '/ExplorerTest14/icons/icon-512.png',
+  'https://aframe.io/releases/1.4.2/aframe.min.js'
 ];
 
+// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Activate Service Worker
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
+// Fetch assets
 self.addEventListener('fetch', event => {
-  // Network-first strategy:
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => {
+      // Return cached asset if available, else fetch from network
+      return response || fetch(event.request);
+    })
   );
 });
