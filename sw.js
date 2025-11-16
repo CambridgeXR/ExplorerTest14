59d60fd24dd453e510c49dabbeebbeeb4a09a8ce
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-16 16:46:20 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,4 +3,4 @@ const urlsToCache=['/','/index.html','/manifest.json','/sw.js','https://aframe.i
 
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); });
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); }); 
