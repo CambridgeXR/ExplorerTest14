6a9cc72a714d7634cce6a84e6691f2c1065d81c5
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-16 16:00:06 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,40 +5,20 @@ const urlsToCache = [
   '/manifest.json',
   '/sw.js',
   'https://aframe.io/releases/1.4.2/aframe.min.js'
-  // Add any logos or other assets here
 ];
 
-// Install event
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate event
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request).catch(() => {
-          // fallback if offline (optional)
-        });
-      })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
