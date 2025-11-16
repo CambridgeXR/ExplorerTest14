42b6b6c78cf6fb01f239c949d5a1764c6cdef00e
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-16 17:23:53 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,29 +2,9 @@ const CACHE_NAME = 'vr-explorer-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/vr.html',
-  '/vr-ipd.html',
   '/manifest.json',
-  '/sw.js',
-  'https://aframe.io/releases/1.5.0/aframe.min.js'
+  '/sw.js'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
