c2a1601a5e7453d1c846745c7ef759db3c2c58ae
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-16 17:12:50 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,40 +2,29 @@ const CACHE_NAME = 'vr-explorer-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/vr.html',
+  '/vr-ipd.html',
   '/manifest.json',
   '/sw.js',
-  // A-Frame library
   'https://aframe.io/releases/1.5.0/aframe.min.js'
 ];
 
-// Install service worker and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate service worker and remove old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
-// Fetch handler: respond with cached resources when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
